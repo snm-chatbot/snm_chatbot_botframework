d231677f25cd051e4e861e66c127a97614d4dfd9
lukther <lukther@gmail.com>
2017-05-08 18:30:29 +0200

    fallback repair
    oprava a doplnění funkčnosti fallbacku + přidána funkce help, která
    vypíše, co bot umí (jen textově bez buttonů)

--- skills/fallback.js
+++ skills/fallback.js
@@ -1,6 +1,47 @@
 var giphy = require('giphy-api')();
 
 module.exports = function(controller) {
+
+    controller.hears(['(help|pomoc(t)*)'], 'message_received', function (bot, message) {
+        var skills = {
+            attachments: [
+                {
+                    contentType: 'application/vnd.microsoft.card.receipt',
+                    content: {
+                        title: 'Umím odpovědět na otázky ohledně:',
+                        items: [
+                            {
+                                title: 'Přijímacího řízení',
+                                subtitle: 'přihláška, přijímačky, atd.'
+                            },
+                            {
+                                title: 'Harmonogramu',
+                                subtitle: 'důležité termíny, rozvrh. atd.'
+                            },
+                            {
+                                title: 'Státnic',
+                                subtitle: 'okruhy, otázky, atd.'
+                            },
+                            {
+                                title: 'Diplomky',
+                                subtitle: 'termíny, témata, atd.'
+                            },
+                            {
+                                title: 'Lidech na stunome',
+                                subtitle: 'vyučující, studenti, atd.'
+                            },
+                            {
+                                title: 'Bonus navíc - umím gify',
+                                subtitle: 'zkus napsat gif + co tě napadne (anglicky) a uvidíš ;)'
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+        bot.reply(message, skills);
+    });
+
     controller.on('message_received', function(bot, message) {
         giphy.random('idk', function (err, res) {
             if (res.data.id) {
@@ -18,7 +59,7 @@ module.exports = function(controller) {
                 };
             }
             var responses = [
-                'Teď ti moc nerozumim.',
+                'Teď ti moc nerozumím.',
                 'Cože??',
                 'Znovu a lépe.',
                 'WTF.',
@@ -26,8 +67,27 @@ module.exports = function(controller) {
                 'tlhIngan Hol vIjatlhlaHbe\'.', //Neumím hovořit klingonsky. :D
             ];
             var response = responses[Math.floor(Math.random() * responses.length)];
-            bot.reply(message, response + ' Zkus něco jiného nebo si vyber v menu vlevo dole.');
+            var help = {
+                attachments: [
+                    {
+                        contentType: 'application/vnd.microsoft.card.hero',
+                        content: {
+                            buttons: [
+                                {
+                                    type: 'postBack',
+                                    title: 'ukaž, co umíš',
+                                    value: 'help'
+                                }
+                            ]
+                        }
+                    }
+                ]
+            };
             bot.reply(message, gif);
+            setTimeout(function() {
+                bot.reply(message, response + ' Zkus něco jiného nebo se podívej, co všechno umím...');
+                bot.reply(message, help);
+            }, 2000);
             return false;
         });
     });
